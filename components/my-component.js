4ca90cce89f3f585e22cbeceff23ac9772cdab44
eolvera <eandrei.olvera@gmail.com>
2021-02-18 01:12:27 -0600

    Primer commit

---
+++ components/my-component.js
@@ -0,0 +1,9 @@
+import {LitElement, html} from 'lit-element';
+
+export class ElementoPropio extends LitElement{
+    render(){
+        return html`
+                <div>Mi primer elemento</div>
+            `;
+    }
+}
---
+++ index.js
@@ -0,0 +1,5 @@
+import { MyElement } from './my-element';
+import { ElementoPropio } from './components/my-component';
+
+window.customElements.define('my-element',MyElement);
+window.customElements.define('ele-propio',ElementoPropio);
